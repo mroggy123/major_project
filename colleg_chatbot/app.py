f7daba4c11cfd47442a9e805b9518e1ed5323e13
Akshay Harilal <akshayharilal10@gmail.com>
2025-03-17 18:55:46 +0530

    javascript link error corrected

--- colleg_chatbot/app.py
+++ colleg_chatbot/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # MySQL Configuration
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = '1234'
 app.config['MYSQL_DB'] = 'college_chatbot'
 
 mysql = MySQL(app)
